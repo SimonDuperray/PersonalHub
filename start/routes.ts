844dc3fc7bf3e4acd39e65fe0aec17a202b0f0ae
Simon <simon.duperray@reseau.eseo.fr>
2022-02-22 09:46:45 +0100

    :hammer: create basics router and views

--- start/routes.ts
+++ start/routes.ts
@@ -20,6 +20,22 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
+// HOMEPAGE
 Route.get('/', async ({ view }) => {
   return view.render('welcome')
+})
+
+// GITHUB DASHBOARD
+Route.get('/github-dashboard', async ({ view }) => {
+  return view.render('github-dashboard/gh-db-form')
+})
+
+// PROJECTS IDEAS
+Route.get('/projects-ideas', async ({ view }) => {
+  return view.render('projects-ideas/pji')
+})
+
+// GOOGLE NEWS
+Route.get('/google-news', async ({ view }) => {
+  return view.render('google-news/google-news')
 })
